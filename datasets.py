b9887ae7a1e2fac591386f5ecd73cb8e965dfb0c
lubomirovna <daria.lazorenko2002@gmail.com>
2022-11-15 17:53:44 +0100

    New version

---
+++ datasets.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import torch
+import torchtext
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm_notebook
+
+df = pd.read_csv('inputs/training_tweets.csv')
+
+
+def create_datasets():
+    """
+    Function to build the training, and test dataset.
+    """
+    X_train, X_test, Y_train, Y_test = train_test_split(df['tweet'].tolist(),
+                                                        df['polarity'].tolist(),
+                                                        test_size=0.3,
+                                                        stratify=df['polarity'].tolist(),
+                                                        random_state=42)
+    train_data = list(zip(X_train, Y_train))
+    test_data = list(zip(X_test, Y_test))
+    print(f"Total training data: {len(train_data)}")
+    print(f"Total validation data: {len(test_data)}")
+    return train_data, test_data
+
+
+tokenizer = torchtext.data.utils.get_tokenizer('basic_english')
+
+
+def yield_tokens(data_iter):
+    for text, _ in data_iter:
+        yield tokenizer(text)
+
+
+def build_vocab(train_data, path=None):
+    """
+    Function to build the vocab from training data.
+    """
+    vocab = torchtext.vocab.build_vocab_from_iterator(yield_tokens(train_data),
+                                                      min_freq=3,
+                                                      specials=["<unk>"])
+    
+    unk_index = vocab['<unk>']
+    vocab.set_default_index(unk_index)
+
+    if (path):
+        torch.save(vocab, path)
+
+    return vocab
--- model.py
+++ model.py
@@ -2,47 +2,73 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class CNN(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, output_dim, 
-                 dropout):
-        
-        super().__init__()
-                
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        
-        self.convs = nn.ModuleList([
-                                    nn.Conv2d(in_channels = 1, 
-                                              out_channels = n_filters, 
-                                              kernel_size = (fs, embedding_dim)) 
-                                    for fs in filter_sizes
-                                    ])
-        
-        self.fc = nn.Linear(len(filter_sizes) * n_filters, output_dim)
-        
-        self.dropout = nn.Dropout(dropout)
-        
-    def forward(self, text):
-                
-        #text = [batch size, sent len]
-        
-        embedded = self.embedding(text)
-                
-        #embedded = [batch size, sent len, emb dim]
-        
-        embedded = embedded.unsqueeze(1)
-        
-        #embedded = [batch size, 1, sent len, emb dim]
-        
-        conved = [F.relu(conv(embedded)).squeeze(3) for conv in self.convs]
-            
-        #conved_n = [batch size, n_filters, sent len - filter_sizes[n] + 1]
-                
-        pooled = [F.max_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved]
-        
-        #pooled_n = [batch size, n_filters]
-        
-        cat = self.dropout(torch.cat(pooled, dim = 1))
 
-        #cat = [batch size, n_filters * len(filter_sizes)]
-            
+class CNN(nn.Module):
+    def __init__(
+            self,
+            pretrained_embedding=None,
+            freeze_embedding=False,
+            vocab_size=None,
+            embedding_dim=200,
+            n_filters=[100, 100, 100],
+            filter_sizes=[2, 3, 4],
+            output_dim=3,
+            dropout=0.5):
+
+        super().__init__()
+
+        if pretrained_embedding is not None:
+            self.vocab_size, self.embedding_dim = pretrained_embedding.shape
+            self.embedding = nn.Embedding.from_pretrained(
+                pretrained_embedding, freeze=freeze_embedding)
+        else:
+            self.embedding = nn.Embedding(vocab_size, embedding_dim)
+
+        self.convs = nn.ModuleList([
+            nn.Conv1d(in_channels=embedding_dim,
+                      out_channels=n_filters,
+                      kernel_size=fs)
+            for fs in filter_sizes
+        ])
+
+        self.fc = nn.Linear(len(filter_sizes) * n_filters, output_dim)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, text):
+
+        # text = [batch size, sent len]
+
+        embedded = self.embedding(text)
+
+        # embedded = [batch size, sent len, emb dim]
+
+        embedded = embedded.permute(0, 2, 1)
+
+        # embedded = [batch size, emb dim, sent len]
+
+        conved = [F.relu(conv(embedded)) for conv in self.convs]
+
+        # conved_n = [batch size, n_filters, sent len - filter_sizes[n] + 1]
+
+        pooled = [F.max_pool1d(conv, conv.shape[2]).squeeze(2)
+                  for conv in conved]
+
+        # pooled_n = [batch size, n_filters]
+
+        cat = self.dropout(torch.cat(pooled, dim=1))
+
+        # cat = [batch size, n_filters * len(filter_sizes)]
+
         return self.fc(cat)
+
+
+def reset_weights(m):
+    '''
+      Try resetting model weights to avoid
+      weight leakage.
+    '''
+    for layer in m.children():
+        if hasattr(layer, 'reset_parameters'):
+            print(f'Reset trainable parameters of layer = {layer}')
+            layer.reset_parameters()
